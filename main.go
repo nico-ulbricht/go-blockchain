77636106b4ada05b74301dbdcdb35a13886c940b
Nico Ulbricht <nico.ulbricht@live.de>
2019-06-12 12:34:14 +0200

    feat: validate entire chain

--- main.go
+++ main.go
@@ -14,18 +14,27 @@ func init() {
 
 func main() {
 	previousBlock := chain.NewGenesis()
+	blockChain := chain.NewChain()
 
 	challenge := uint(0)
 	for {
 		block := chain.New(previousBlock, challenge)
 		isValid := block.IsSolutionValid()
 		fmt.Println(challenge, block.GetHash(), isValid)
-		if isValid == true {
-			previousBlock = block
-			challenge = 0
-			time.Sleep(time.Second * 2)
+		if isValid == false {
+			challenge++
+			continue
 		}
 
-		challenge++
+		previousBlock = block
+		blockChain.AddBlock(block)
+		challenge = 0
+		if blockChain.GetLength() == 3 {
+			break
+		}
+
+		time.Sleep(time.Second * 2)
 	}
+
+	fmt.Printf("chain validity: %v\n", blockChain.IsChainValid())
 }
--- pkg/chain/chain.go
+++ pkg/chain/chain.go
@@ -1,4 +1,34 @@
 package chain
 
 // Chain .
-type Chain []Block
+type Chain interface {
+	AddBlock(b Block) Block
+	IsChainValid() bool
+	GetLength() int
+}
+
+type chain []Block
+
+func (c *chain) AddBlock(b Block) Block {
+	addedChain := append(*c, b)
+	*c = addedChain
+	return b
+}
+
+func (c *chain) GetLength() int {
+	return len(*c)
+}
+
+func (c *chain) IsChainValid() bool {
+	for _, aBlock := range *c {
+		if aBlock.IsSolutionValid() == false {
+			return false
+		}
+	}
+
+	return true
+}
+
+func NewChain() Chain {
+	return &chain{}
+}
